7ae17b5bddd2b04b42131873bdbfe8a8aa3d08f8
DanM <dan.b.mann@gmail.com>
2023-08-20 17:30:30 -0700

    Update interface.py
    updated steer delay

--- selfdrive/car/subaru/interface.py
+++ selfdrive/car/subaru/interface.py
@@ -79,7 +79,7 @@ class CarInterface(CarInterfaceBase):
       ret.wheelbase = 2.67
       ret.centerToFront = ret.wheelbase * 0.5
       ret.steerRatio = 18.984           # learned, 14 stock
-      ret.steerActuatorDelay = 0.1
+      ret.steerActuatorDelay = 0.2
       ret.steerLimitTimer = 0.9
       CarInterfaceBase.configure_torque_tune(candidate, ret.lateralTuning)
       #ret.lateralTuning.init('pid')
